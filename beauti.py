d320457ba71be2babf28dfc924ca99be31b6a040
heejaee <83530474+heejaee@users.noreply.github.com>
2021-05-25 16:15:46 +0900

    Update beauti.py

--- beauti.py
+++ beauti.py
@@ -1,27 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = "https://www.gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358"
-headers={"User-Agent":"Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36 (Gecko와 같은 KHTML) Chrome / 90.0.4430.212 Safari / 537.36"}
-res = requests.get(url, headers)
-res.raise_for_status()
-
+res = requests.get(url)
 soup = BeautifulSoup(res.text, "lxml")
-# rank = soup.find("tbody")
+
 ranks = soup.find_all("td", attrs = {"class":"tl"})
-# print(rank.get_text())
-# title = ranks[0].a.get_text()
-# link = ranks[0].a["href"]
-# print(title)
-# print("https://www.gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358"+link)
-for rank in ranks:
-    title = rank.a.get_text()
-    link = rank.a["href"]
-    # print(title)
-    # print("https://www.gachon.ac.kr/community/opencampus/03.jsp?boardType_seq=358"+link)
-
-
+link2="https://www.gachon.ac.kr/community/opencampus/"
+# titles = soup.select('.boardlist>tbody>tr>td')
 import telegram
 토큰 = "1835518048:AAFzzz7WNfrdGD1bG8sHdl5sE4fltlK3LUE"
 봇 = telegram.Bot(token=토큰)
@@ -30,4 +16,10 @@ import telegram
 # for i in 봇.getUpdates():
 #     print(i.message)
 
-봇.send_message(1895249171,title)    
+
+for rank in ranks:
+    title = '<가천대 게시글 업데이트>' + '\n'+rank.a.get_text().strip()
+    link = rank.a["href"]
+    finallink=link2+link
+    봇.send_message(1895249171,title+finallink)
+
